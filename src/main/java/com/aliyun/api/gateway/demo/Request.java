82abb57186c550e3900247a1e1bb89a0d8e51afb
aicodeguard <zifeiyu19980606@gmail.com>
2025-11-10 17:35:26 +0800

    Signed-off-by: aicodeguard <zifeiyu19980606@gmail.com>

--- src/main/java/com/aliyun/api/gateway/demo/Request.java
+++ src/main/java/com/aliyun/api/gateway/demo/Request.java
@@ -199,7 +199,4 @@ public class Request {
     public void setSignHeaderPrefixList(String signHeaderPrefixList) {
         this.signHeaderPrefixList = Arrays.asList(signHeaderPrefixList.split(","));
     }
-    public void addSignHeaderPrefix(String signHeaderPrefix) {
-        this.signHeaderPrefixList.add(signHeaderPrefix);
-    }
 }
